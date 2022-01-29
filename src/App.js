5f84b8faad4bbb654b62bea195aaca33318e2f90
Philip-Nicolas <philnic.varga@mail.utoronto.ca>
2022-01-29 10:27:18 -0500

    proper tile layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 // region Mocked Dependencies
 
 const getLetters = () => "abcdef";
 
 const getLanguage = () => "fr";
+
+// endregion
+
+// region Utils
+
+const getLineHeightForHeight = (height) => {
+  return Math.round(height * 2 / 3)
+}
 
 // endregion
 
@@ -26,35 +34,67 @@ function Row(props) {
   )
 }
 
+function Square(props) {
+  return (
+    <div className="SquareContentContainer" style={{ width: props.width || "auto" }}>
+      <svg viewBox="0 0 1 1"/>
+      {props.children}
+    </div>
+  )
+}
+
 // endregion
+
+function Tile(props) {
+  return (
+    <div className="Tile" onClick={props.onClick}>
+      {props.children}
+    </div>
+  );
+}
 
 
 function WordEntryArea(props) {
   const letters = props.letters;
 
   const tileRenderers = letters.map(letter => {
-    return (onClick) => (
-      <div className="Tile" onClick={onClick}>
-        {letter}
-      </div>
-    );
+    return function (onClick) {
+      // let style = {};
+      //
+      // if (clickedTile) {
+      //   const dx = clickedTile.offsetLeft;
+      //   const dy = clickedTile.offsetHeight;
+      //   style.transform = `translate(-${dx}px, -${dy}px)`;
+      //   setTimeout(() => {
+      //     style.transform = 'translate(0, 0)'
+      //   }, 500);
+      // }
+
+      return (
+        <Tile onClick={onClick}>
+          {letter}
+        </Tile>
+      );
+    }
   })
 
   const [activeTiles, setActiveTiles] = useState([...letters.keys()]);
   const [inactiveTiles, setInactiveTiles] = useState(new Array(letters.length).fill(null));
+  const [clickedTile, setClickedTile] = useState(null);
 
   /**
    * handles click event for active tiles.
    * Tile specified by index in the `tileSpaces.active` array.
    */
   const getOnClickActiveTile = (index) => {
-    return () => {
+    return (e) => {
       const active = [...activeTiles];
       const inactive = [...inactiveTiles];
 
       inactive[activeTiles[index]] = active.splice(index, 1)[0];
       active.push(null);
 
+      setClickedTile(e.target)
       setActiveTiles(active);
       setInactiveTiles(inactive);
     }
@@ -77,9 +117,13 @@ function WordEntryArea(props) {
     return tiles.map((tileIndex) => {
       return (index) => {
         return (
-          <div className="HeightBasedSquare TileSpace">
-            {tileIndex == null ? null : tileRenderers[tileIndex](onClickFactory(index))}
-          </div>
+          <Square className="TileSpace" width="6rem">
+            <div className="TileSpace" style={{
+              "font-size": "4em",
+            }}>
+              {tileIndex == null ? null : tileRenderers[tileIndex](onClickFactory(index))}
+            </div>
+          </Square>
         )
       }
     })
